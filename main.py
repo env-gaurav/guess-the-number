2d4e9416f108d01f05249d657240bac277247ee3
env-gaurav <gaurav2528singh@gmail.com>
2025-02-02 16:02:38 +0530

    bugfix: Incomplete validation

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ attempts = 2
 
 while True:
     user_input = input("Guess the Number: ")
-    if int(user_input) == answer:
-        print("You Won!")
-        break
-    elif int(user_input) != answer:
-        if attempts != 0:
-            print(f"Match Failed, try again {attempts} attempts left.")
-            attempts -= 1
-        else:
-            print("You Lost!")
+    try:
+        if int(user_input) == answer:
+            print("You Won!")
             break
-    else:
-        print("Invalid Input, Enter numbers only.")
+        elif int(user_input) != answer:
+            if attempts != 0:
+                print(f"Match Failed, try again {attempts} attempts left.")
+                attempts -= 1
+            else:
+                print("You Lost!")
+                break
+
+    except ValueError:
         if attempts != 0:
-            print("Try again, {attempts} attempts left.")
-            attempts -= 1
+                print("Try again, {attempts} attempts left.")
+                attempts -= 1
         else:
             print("You Lost")
             break
