ee67f90aee035eb155689851e9f66ec3093adbb5
env-gaurav <gaurav2528singh@gmail.com>
2025-02-02 15:29:55 +0530

    input validation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 answer = 5
-user_input=int(input("ENTER YOUR NUMBER: "))
-if answer == user_input:
+user_input=input("ENTER YOUR NUMBER: ")
+if type(user_input) != int:
+    print("user input must be interger")
+if user_input == answer:
     print("you won")
 else:
     print("you lost")
